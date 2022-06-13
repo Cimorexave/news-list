c0189ce566902cf0d31befc9e989059d9cc7df1a
Cimorexave <sadeq.s502@gmail.com>
2022-06-13 16:58:50 +0430

    finished functionality and most of the styling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import { lineHeight } from '@mui/system';
+
 
 
 
@@ -72,7 +72,7 @@ const App = () => {
     }
     if (keyword != "" && date != "" && url != "") setUrlIsValid(true)
     if (urlIsValid) {
-      //fetchData()
+      fetchData()
       console.log( items)
     }
   },[url])
@@ -86,7 +86,7 @@ const App = () => {
         setKeyword = {setKeyword}
         totalArticles= {items.totalArticles} />
       </div>
-      
+      {/*
       <div className="articles-list">
       <Card sx={{padding: 2, backgroundColor: '#F2F2F2'}} >
         <CardMedia 
@@ -123,16 +123,34 @@ const App = () => {
         </CardActions>
       </Card>
       </div>
+      */}
       { 
-      /*
+      
         items.articles.map((article)=>{
           return(
-            <div className="articles-list">
-              <Card />
+            <div key={Math.random()*items.articles.length} className="articles-list">
+              <Card  sx={{padding: 2, backgroundColor: '#F2F2F2'}} >
+        <CardMedia 
+        sx={{borderRadius: 3}}
+        component='img'
+        image={article.image}
+        alt='alternate image'
+        />
+        <CardContent sx={{paddingTop: 2, paddingBottom: 1, paddingRight: 0, paddingLeft: 0}} >
+          <Typography variant='h4' style={{fontSize: 18, fontWeight:'bold', marginBottom: 5}} >{article.title}</Typography>
+          <Typography variant='body2' style={{fontSize: 12}}>{article.description}</Typography>
+          <Typography variant='caption' sx={{fontSize: 8}} > source : {article.source.name} @ 
+          <Link sx={{color: 'orange', lineHeight: '10%'}} href={article.url} > {article.url} </Link> </Typography>
+          <Typography variant='caption' sx={{fontSize: 8}} >  / Date : {article.publishedAt} </Typography>
+        </CardContent>
+        <CardActions sx={{padding: 0}}>
+          <Button variant='contained' size='small' color='warning'  >Read More</Button>
+        </CardActions>
+      </Card>
             </div>
           )
         })
-        */
+        
       }
       <BottomNavigation>
         <BottomNavigationAction />
