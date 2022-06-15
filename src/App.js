926a2b66ecea794e44da211db4d93b77468562c2
Cimorexave <sadeq.s502@gmail.com>
2022-06-15 15:07:46 +0430

    removed unused development code

--- src/App.js
+++ src/App.js
@@ -14,24 +14,6 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 
-
-
-
-const articleExample = {
-  "title": "Nick Leeder appointed as latest head of Google Ireland",
-  "description": "Google has announced that Nick Leeder will replace Fionnuala Meehan as the head of its Irish operation starting in April.",
-  "content": "Google has announced that Nick Leeder will replace Fionnuala Meehan as the head of its Irish operation starting in April.\nWhile its staff continue to work from home in the midst of the coronavirus pandemic, Google Ireland will have a new person leadi... [1514 chars]",
-  "url": "https://www.siliconrepublic.com/companies/nick-leeder-google-ireland",
-  "image": "https://www.siliconrepublic.com/wp-content/uploads/2020/03/BOO_3353_2.jpg",
-  "publishedAt": "2020-03-23T13:58:53Z",
-  "source": {
-      "name": "Silicon Republic",
-      "url": "https://www.siliconrepublic.com/"
-  }
-}
-
-
-
 const apiToken = '8c522ab951da88162a3e3a27b39424ab'
 const App = () => {
   const [date, setDate] = useState("")
